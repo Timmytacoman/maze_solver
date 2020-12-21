4dd5e837dd1d848ce8b5e71f26a66010259aa093
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2020-12-21 01:38:52 -0700

    various new ideas implemented

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -------------------------------------------
-# Maze Solver V1.0
+# Maze Solver V2.0
 # Author: Timothy Foreman
 # Date last modified: 12/20/2020
 # -------------------------------------------
@@ -9,193 +9,170 @@ import numpy as np
 import pygame
 import time
 
+IMAGE_REF = "images/maze (2).gif"
+IMAGE_SCALE = 10
+START_COLOR = (100, 255, 100)
+FINISH_COLOR = (100, 100, 255)
+PATH_COLOR = (255, 100, 100)
+
 
 # -------------------------------------------
-
 
 class Pixel:
     """Pixel class for each pixel in the maze image"""
 
-    entrances = []
-
-    def __init__(self, x, y, color, is_entrance):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = color
-        self.is_entrance = is_entrance
+        self.color = color  # 0: White | 1: Black
 
     def __str__(self):
-        return f"x: {self.x}\ny: {self.y}\ncolor: {self.color}\nis_entrance: {self.is_entrance}\n"
+        return f"Pixel: [{self.x}, {self.y}] | Color: {self.color}"
 
 
 # -------------------------------------------
-maze_image = "images/maze (1).gif"
-im = Image.open(maze_image)
-pix = im.load()
-width, height = im.size
 
-# create 2d array of Pixel objects, INCLUDING WHITE LINE
+# Setup image
+image = Image.open(IMAGE_REF)
+image_width, image_height = image.size
+color_value = image.load()
+
+# Lists to hold Pixel objects
+entire_col = []
 grid = []
-line = []
+entrances = []
 
-# white is 0
+# Collect image data, marking entrances as such
+for _x in range(image_width):
+    for _y in range(image_height):
+        pixel = Pixel(_x, _y, color_value[_x, _y])
+        entire_col.append(pixel)
+        if color_value[_x, _y] == 0:
+            if (_x == 1 or _x == image_width - 2) and (_y != 0 and _y != image_height - 1):
+                entrances.append(pixel)
+            if (_y == 1 or _y == image_width - 2) and (_x != 0 and _x != image_height - 1):
+                entrances.append(pixel)
 
+    grid.append(entire_col)
+    entire_col = []
 
-for row in range(height):
-    for col in range(width):
-        # x = col, y = row
-        # get color
-        color = pix[col, row]
-        # check if is entrance on border
-        is_entrance = False
-        if color == 0:
-            if (col != 0 and col != width - 1) and (row != 0 and row != height - 1):
-                if (col == 1 or col == width - 2) or (row == 1 or row == height - 2):
-                    # is entrance
-                    is_entrance = True
-        # add to line
-        line.append(Pixel(col, row, color, is_entrance))
-        # append to separate list for entrances
-        if is_entrance:
-            Pixel.entrances.append(Pixel(col, row, color, is_entrance))
-    # add line row to grid
-    grid.append(line)
-    # reset line
-    line = []
-
-# convert to numpy array
-grid = np.array(grid)
-
-# -------------------------------------------
-
-
-# print(Pixel.entrances[0], Pixel.entrances[1])
-# use pygame?
-
-# 825 x 500 matches preview
-window_width = 825
-window_height = 500
-
-pygame.init()
-# window = pygame.display.set_mode((width, height))
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("maze_solver")
-clock = pygame.time.Clock()
-
-image = pygame.image.load(maze_image)
-image_width, image_height = image.get_size()
-big_image = pygame.transform.scale(image, (window_width, window_height))
-
-scale = window_width / image_width
-
-window.blit(big_image, (0, 0))
+# Declarations
+start = entrances[0]
+finish = entrances[1]
 
 
 # -------------------------------------------
-
-
-def get_surrounding(pixel):
-    x_pos = pixel.x
-    y_pos = pixel.y
-    # print(x_pos, y_pos)
-
-    # north = grid[row][col]
-    north = grid[y_pos - 1][x_pos]
-    east = grid[y_pos][x_pos + 1]
-    south = grid[y_pos + 1][x_pos]
-    west = grid[y_pos][x_pos - 1]
-
-    return [north, east, south, west]
-
 
 def draw_pixel(x, y, color):
-    pygame.draw.rect(window, color, (x * scale, y * scale, scale, scale))
+    pygame.draw.rect(display_surface, color, (x * IMAGE_SCALE, y * IMAGE_SCALE, IMAGE_SCALE, IMAGE_SCALE))
 
 
-def check_surrounding_paths(pixel):
-    adjacent = get_surrounding(pixel)
-    open_paths = []
-    for i in adjacent:
-        if i.color == 0 and (i.x != 0 and i.x != width - 1) and (
-                i.y != 0 and i.y != height - 1):  # white and not border
-            open_paths.append(i)
-
-    return open_paths
+def is_at_finish(pixel1):
+    if (pixel1.x == finish.x) and (pixel1.y == finish.y):
+        return True
+    return False
 
 
-# possibilities = check_surrounding_paths(Pixel.entrances[0])
-# for i in possibilities:
-#     print(i)
-#     draw_pixel(i.x, i.y, (0, 255, 255))
+def find_viable_paths(pixel):
+    # Setup search for paths
+    viable_paths = []
+    x_pos = pixel.x
+    y_pos = pixel.y
+    north = grid[x_pos][y_pos - 1]
+    east = grid[x_pos + 1][y_pos]
+    south = grid[x_pos][y_pos + 1]
+    west = grid[x_pos - 1][y_pos]
+    directions = [north, south, east, west]
+
+    # Check color
+    for i in directions:
+        if i.color == 0:
+            viable_paths.append(i)
+
+    # Remove borders
+    for i in viable_paths:
+        if i.x == 0 or i.y == 0:
+            viable_paths.remove(i)
+
+    return viable_paths
 
 
-draw_pixel(Pixel.entrances[0].x, Pixel.entrances[0].y, (0, 255, 0))
-draw_pixel(Pixel.entrances[1].x, Pixel.entrances[1].y, (0, 255, 0))
+# def search(pixel):
+#     pathing = []
+#     current_position = pixel
+#     paths = [1]
+#     while len(paths) == 1:
+#         paths = find_viable_paths(current_position)
+#
+#         # continue along straight path
+#         pathing.append(current_position)
+#         current_position = paths[0]
+#         draw_pixel(current_position.x, current_position.y, PATH_COLOR)
 
-current_pixel = Pixel.entrances[0]
+def solve():
+    global behind
+    global pos
 
-previous_positions = []
-splits = []
-current_successful_path = []
+    # Base case
+    if is_at_finish(pos):
+        print("YAY!!!!!!!")
 
-crashed = False
+    # Setup paths
+    paths = find_viable_paths(pos)
 
-while not crashed:
+    # Remove behind
+    if behind is not None:
+        for i in paths:
+            if (i.x, i.y) == (behind.x, behind.y):
+                paths.remove(i)
 
+    # Move along straight line
+    if len(paths) == 1:
+        behind = pos
+        pos = paths[0]
+        draw_pixel(pos.x, pos.y, PATH_COLOR)
+
+
+
+    else:
+        print(len(paths))
+
+
+
+
+
+
+# -------------------------------------------
+
+# Pygame setup
+pygame.init()
+surface_width = image_width * IMAGE_SCALE
+surface_height = image_height * IMAGE_SCALE
+display_surface = pygame.display.set_mode((surface_width, surface_height))
+pygame.display.set_caption("MAZE SOLVER")
+clock = pygame.time.Clock()
+
+# Image setup
+pg_image = pygame.image.load(IMAGE_REF)
+big_image = pygame.transform.scale(pg_image, (surface_width, surface_height))
+display_surface.blit(big_image, (0, 0))
+
+# Draw entrances
+draw_pixel(start.x, start.y, START_COLOR)
+draw_pixel(finish.x, finish.y, FINISH_COLOR)
+
+behind = None
+pos = start
+
+# Game loop
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            running = False
+    solve()
 
-        # print(event)
-
+    clock.tick(10)
     pygame.display.update()
-    clock.tick(60)
 
-    possibilities = check_surrounding_paths(current_pixel)
-
-    for i in possibilities:
-        if (i.x, i.y) in previous_positions:  # remove spot that I came from
-            possibilities.remove(i)
-
-    if len(possibilities) == 1:  # one possible path
-        # print("making move")
-        previous_positions.append((current_pixel.x, current_pixel.y))  # add previous position into memory
-        current_pixel = possibilities[0]  # move to open spot
-        draw_pixel(current_pixel.x, current_pixel.y, (255, 0, 0))  # draw pixel on current spot
-
-    elif len(possibilities) == 2:
-        print("Fork")
-        # arrived at fork
-        # keep track of journey
-        current_successful_path.append(previous_positions)
-
-        # mark confirmed path with a different color
-        for i in previous_positions:
-            # print(i)
-            draw_pixel(i[0], i[1], (100, 200, 0))
-
-        # reset previous positions
-        previous_positions = [previous_positions[-1]]
-
-        splits.append((current_pixel.x, current_pixel.y))
-
-        # select a path to continue through
-        previous_positions.append((current_pixel.x, current_pixel.y))  # add previous position into memory
-        current_pixel = possibilities[1]
-        previous_positions.append((current_pixel.x, current_pixel.y))  # add previous position into memory
-        draw_pixel(current_pixel.x, current_pixel.y, (255, 0, 0))  # draw pixel on current spot
-
-
-
-    elif len(possibilities) == 0:
-        print("Dead end")
-        # dead end, reset failed track color
-        previous_positions.append((current_pixel.x, current_pixel.y))
-        for i in previous_positions:
-            draw_pixel(i[0], i[1], (255, 255, 255))
-        previous_positions = previous_positions[:3]
-
-        # move to fork
-        current_pixel = grid[splits[0][1]][splits[0][0]]
-        draw_pixel(current_pixel.x, current_pixel.y, (133, 25, 0))
-
+# -------------------------------------------
